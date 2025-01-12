e81782940447aa836622eabe73a424857d7483e6
Chris Pawley <christopher.pawley@gmail.com>
2025-01-12 16:48:57 +0100

    Add SearchRequest test

---
+++ test/core/search.request.spec.ts
@@ -0,0 +1,31 @@
+
+import 'dotenv/config'
+import ky from 'ky'
+import { LinkedInClient } from '../../src/index.js'
+import { ConsoleLogger } from '../../src/utils/logger/console-logger.js'
+import { writeJsonToFile } from './test-util.js'
+
+describe('SearchRequest', () => {
+  let linkedin: LinkedInClient
+
+  beforeAll(async () => {
+    linkedin = new LinkedInClient({
+      logger: new ConsoleLogger(),
+      ky: ky.extend({
+        /*
+        dispatcher: new EnvHttpProxyAgent() as any,
+        */
+      })
+    })
+
+    await linkedin.ensureReady()
+  })
+
+  it("searchPeopleByTitle", async () => {
+    const res = await linkedin.search.searchPeople('Chris Pawley')
+    await writeJsonToFile(res, `./test/example/search/people-search-title-chris_pawley.json`);
+
+  }, 30_000)
+
+
+})
